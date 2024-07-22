aef702ff34fa3e02a597bbd899a8c36b0a246300
manhhoach <manhhn.nv5@gmail.com>
2024-07-22 21:46:08 +0700

    init

---
+++ src/ProductManagement.Domain/Products/Products.cs
@@ -0,0 +1,21 @@
+﻿using ProductManagement.Categories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace ProductManagement.Products
+{
+    public class Products: FullAuditedAggregateRoot<Guid>
+    {
+        public Category Category { get; set; }
+        public Guid CategoryId { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public bool IsFreeCargo { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public ProductStockState StockState { get; set; }
+    }
+}
